b16ee19c9a13c54c4c9576179e958936081308f1
A00932944 <A00932944@SE12L0320-03.ad.bcit.ca>
2016-01-07 11:18:07 -0800

    Second commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html: charset=UTF-8">
+        <title>Lab 01</title>
+    </head>
+    <body>
+        <?php
+        echo 'My App is here';
+        ?>
+    </body>
+</html>
+
+
